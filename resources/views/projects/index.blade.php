eb3853b714d8a21075a9b4fe107161bb08d375a2
Mike Maltby <maltby.mike@gmail.com>
2019-01-21 21:35:19 -0500

    Laravel from Scratch Completed Episode 8

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Projects</h1>
+
+    @foreach ($projects as $project)
+
+      <li>{{ $project->title }}</li>
+
+    @endforeach
+  </body>
+</html>
