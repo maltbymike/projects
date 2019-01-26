094d520ae0e33dbcde0dd37df57be55eed15be3a
Mike Maltby <maltby.mike@gmail.com>
2019-01-25 22:46:50 -0500

    Laravel from Scratch Completed Episode 20

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -11,20 +11,33 @@
   </div>
 
   @if ($project->tasks->count())
+
     <div class="box">
+
       @foreach ($project->tasks as $task)
+
         <div>
-          <form method="POST" action="/tasks/{{ $task->id }}">
-            @method('PATCH')
+
+          <form method="POST" action="/completed-tasks/{{ $task->id }}">
+            @if ($task->completed)
+              @method('DELETE')
+            @endif
+
             @csrf
+
             <label class='checkbox {{ $task->completed ? 'is-complete' : '' }}' for='completed'>
               <input type='checkbox' name='completed' onChange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}/>
               {{ $task->description }}
             </label>
+
           </form>
+
         </div>
+
       @endforeach
+
     </div>
+
   @endif
 
   <!-- Add a new task form -->
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,6 @@ Route::resource('projects', 'ProjectsController');
 
 Route::post('/projects/{project}/tasks', 'ProjectTasksController@store');
 Route::patch('/tasks/{task}', 'ProjectTasksController@update');
+
+Route::post('/completed-tasks/{task}', 'CompletedTasksController@store');
+Route::delete('/completed-tasks/{task}', 'CompletedTasksController@destroy');
